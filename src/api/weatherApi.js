aa002ec91af4877ace6f093c37cf4d3e9aa8af75
Dimitris Avgoustopoulos <106197029+dimitrisAvgoustopoulos@users.noreply.github.com>
2025-06-12 00:17:38 +0300

    Update weatherApi.js

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 export async function getWeatherData(city) {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
 
   const response = await fetch(url);
   if (!response.ok) {
